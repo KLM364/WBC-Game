8e0124de336511e07674136a3ed45d972dc289f6
KLM364 <g18beatz@hotmail.com>
2016-09-10 10:20:12 +0530

    Made Explosions

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -3,11 +3,18 @@ using System.Collections;
 
 public class DestroyByContact : MonoBehaviour {
 
-	void OnTriggerEnter(Collider other)
+    public GameObject explosion;
+    public GameObject playerExplosion;
+    void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Boundary")
         {
             return;
+        }
+        Instantiate(explosion, transform.position, transform.rotation);
+        if(other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
         }
         Destroy(other.gameObject);
         Destroy(gameObject);
