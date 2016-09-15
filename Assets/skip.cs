4b395d8cc524d5410e8e5e8887a4772f5f71872b
abyrkz <abyrkz@gmail.com>
2016-09-15 21:20:58 +0530

    Added Story and Pre-game Menu

---
+++ Assets/skip.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class skip : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	public void ChangeScene(string scenename)
+	{
+		SceneManager.LoadScene("Menu");
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
